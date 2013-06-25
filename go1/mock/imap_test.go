876a88b63c155ed97b093821876fcd9d17e92a21
Maxim Khitrov <max@mxcrypt.com>
2013-06-24 22:03:12 -0400

    Use a different package name in the mock unit tests.

--- go1/mock/imap_test.go
+++ go1/mock/imap_test.go
@@ -2,21 +2,22 @@
 // Written by Maxim Khitrov (June 2013)
 //
 
-package mock
+package mock_test
 
 import (
 	"testing"
 
 	"code.google.com/p/go-imap/go1/imap"
+	"code.google.com/p/go-imap/go1/mock"
 )
 
 func TestNewClientOK(T *testing.T) {
-	C, t := Client(T,
+	C, t := mock.Client(T,
 		`S: * OK Test server ready`,
 		`C: A1 CAPABILITY`,
 		`S: * CAPABILITY IMAP4rev1 XYZZY`,
 		`S: A1 OK Thats all she wrote!`,
-		EOF,
+		mock.EOF,
 	)
 	t.CheckState(imap.Login)
 	t.CheckCaps("IMAP4rev1", "XYZZY")
